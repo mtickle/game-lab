cd1541f788b79218b850e4cd90ee3b79cb709748
Mike Tickle <mtickle@gmail.com>
2025-08-04 06:23:20 -0400

    Initial

---
+++ src/layouts/Header.jsx
@@ -0,0 +1,13 @@
+import { useAuth0 } from '@auth0/auth0-react';
+const Header = () => {
+    const { loginWithRedirect, logout, isAuthenticated, user, isLoading } = useAuth0();
+
+    return (
+        <div className="bg-gradient-to-r from-blue-600 to-indigo-700 text-white px-6 py-4 shadow-md flex justify-between items-center rounded-t-2xl">
+            <h1 className="text-2xl font-semibold tracking-tight">🎲 Game Lab</h1>
+        </div>
+    );
+}
+
+
+export default Header;
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,26 @@
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss()],
   base: "/game-lab",
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@layouts': path.resolve(__dirname, 'src/layouts'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@styles': path.resolve(__dirname, 'src/styles'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+      '@config': path.resolve(__dirname, 'src/config'),
+      '@app': path.resolve(__dirname, 'src/app'),
+      '@features': path.resolve(__dirname, 'src/features'),
+    },
+  },
 })
+
